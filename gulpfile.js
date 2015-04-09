a686bea9be0959de91796bc399cee4a125f70a8d
Gullit Miranda <gullitmiranda@gmail.com>
2015-04-09 17:31:39 -0300

    Add `./bin` folder to `gulp lint`

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var azk_gulp = require('azk-dev/gulp')({
   cwd  : __dirname,
-  // lint: [ "bin/**/*.js" ], // Extra files for the lint analyzer
+  lint: [ "bin/**/*.js" ], // Extra files for the lint analyzer
 });
 
 var gulp = azk_gulp.gulp;
